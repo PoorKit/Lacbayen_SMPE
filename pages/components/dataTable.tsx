7ded22d2e5dbdc38d5a06c53a4b8b5bb52835a5f
PoorKit <70286339+PoorKit@users.noreply.github.com>
2023-08-15 14:13:24 +0800

    1 file modified, 1 added, 2 deleted

--- pages/components/dataTable.tsx
+++ pages/components/dataTable.tsx
@@ -1,19 +1,48 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import DataTable from 'react-data-table-component';
 const DynamicDataTable = () => {
   const [activeDataSet, setActiveDataSet] = useState('Customer');
 
-  const dataSets = {
-    Customer: [], // Your customer data array
-    Packages: [], // Your packages data array
-    Warehouse: [], // Your warehouse data array
-  };
+  const [dataSets, setDataSets] = useState({
+    Customer: [],
+    Packages: [],
+    Warehouse: [],
+  });
 
-  const columns = [];
+  const [columns, setColumns] = useState([]);
 
   const handleDataSetChange = (dataSetName) => {
     setActiveDataSet(dataSetName);
   };
+
+  useEffect(() => {
+    // Fetch data from the API
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`/api/${activeDataSet}`);
+        const data = await response.json();
+        setDataSets((prevDataSets) => ({
+          ...prevDataSets,
+          activeDataSet: data,
+        }));
+
+        // Infer columns from the first data item
+        if (data.length > 0) {
+          const inferredColumns = Object.keys(data[0]).map((key) => ({
+            name: key,
+            selector: key,
+            sortable: true,
+          }));
+          setColumns(inferredColumns);
+        }
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <>
       <div className="flex space-x-4">
@@ -30,6 +59,7 @@ const DynamicDataTable = () => {
         ))}
       </div>
       <DataTable
+        title={`${activeDataSet} Data`}
         data={dataSets[activeDataSet]}
         columns={columns}
         pagination
